33131ff384981d0d59e05f72ad45ef7fd882abd6
PierreLepesant <pierre.lepesant@icilundi.fr>
2025-07-01 15:41:21 +0200

    todo checker script

--- scripts/todo_checker.sh
+++ scripts/todo_checker.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# grep 'TODO' -r --exclude=../scripts/todo_checker.sh ../ > todos.txt
-# readarray -t todos < <( grep 'TODO' -r ../ )
-
-readarray -t todos < <(grep 'TODO' -r ../)
-
-echo ${todos[@]}
-# for todo in ${todos[@]}
-# do
-#    echo ${todo}
-# done
+while read todo; do
+    exit 1
+done < <(grep 'TODO' -r ../ --exclude-dir=scripts | grep -v 'TODO (' )
